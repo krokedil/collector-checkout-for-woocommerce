4cc5c93e087eecb21cd37971c01240763287a0e1
MichaelBengtsson <michael@krokedil.se>
2017-06-20 10:44:03 +0200

    Started adding process payment function

--- classes/class-collector-bank-gateway.php
+++ classes/class-collector-bank-gateway.php
@@ -3,7 +3,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 class Collector_Bank_Gateway extends WC_Payment_Gateway {
-	private $public_token = '';
 	public function __construct() {
 		$this->id                 = 'collector_bank';
 		$this->method_title       = __( 'Collector Bank', 'collector-bank-for-woocommerce' );
@@ -14,36 +13,133 @@ class Collector_Bank_Gateway extends WC_Payment_Gateway {
 		$this->init_form_fields();
 		// Load the settings.
 		$this->init_settings();
-		if ( is_checkout() ) {
-			//wp_enqueue_script( 'collector_script', 'https://checkout-uat.collector.se/collector-checkout-loader.js', array( 'jquery' ) );
-			//add_filter( 'script_loader_src', array( $this, 'add_data_tags' ), 10, 2 );
-			//add_filter( 'clean_url', array( $this, 'unclean_url' ), 10, 3 );
-		}
 
 		add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
+		if ( isset( $_GET['payment_successful'] ) && WC()->session->get( 'order_awaiting_payment' ) == $_GET['payment_successful'] && is_checkout() ) {
+			error_log( 'in if' );
+			add_filter( 'woocommerce_checkout_get_value', array( $this, 'populate_fields' ), 10, 2 );
+			add_filter( 'woocommerce_checkout_fields', array( $this, 'set_not_required' ), 20 );
+		}
+		//add_action( 'woocommerce_before_checkout_form', array( $this, 'maybe_process_payment' ) );
+
 	}
 	public function init_form_fields() {
 		$this->form_fields = include( COLLECTOR_BANK_PLUGIN_DIR . '/includes/collector-bank-settings.php' );
 	}
 
-	public function add_data_tags( $src, $handle ) {
-		if ( 'collector_script' != $handle ) {
-			return $src;
+	public function maybe_process_payment() {
+		if ( isset( $_GET['payment_successful'] ) && WC()->session->get( 'order_awaiting_payment' ) == $_GET['payment_successful'] && is_checkout() ) {
+			error_log( 'in maybe process payment' );
+			add_filter( 'woocommerce_checkout_get_value', array( $this, 'populate_fields' ), 10, 2 );
+			add_filter( 'woocommerce_checkout_fields' ,  array( $this, 'set_not_required' ), 20 );
+
 		}
-		$init_checkout = new Collector_Bank_Requests_Initialize_Checkout();
-		$request = $init_checkout->request();
-		$decode = json_decode( $request['body'] );
-		$this->public_token = $decode->data->publicToken;
-		return $src . ' data-token="' . $this->public_token . '" data-lang="sv"';
+	}
+	public function populate_fields( $value, $key ) {
+		error_log( 'in populate fields' );
+		// Get information about order from Collector
+		$order_id      = WC()->session->get( 'order_awaiting_payment' );
+		$private_id    = get_post_meta( $order_id, '_collector_private_id' );
+		$customer_data = new Collector_Bank_Requests_Get_Checkout_Information( $order_id, $private_id[0] );
+		$customer_data = $customer_data->request();
+		error_log( var_export( $customer_data, true ) );
+		// Get billing information from the customer
+		$billing_first_name = $customer_data->customer->billigAddress->firstName;
+		$billing_last_name  = $customer_data->customer->billigAddress->lastName;
+		$billing_country    = $customer_data->customer->billigAddress->country;
+		$billing_address    = $customer_data->customer->billigAddress->address;
+		$billing_city       = $customer_data->customer->billigAddress->city;
+		$billing_postcode   = $customer_data->customer->billigAddress->postalCode;
+
+		// Get shipping information from the customer
+		$shipping_first_name = $customer_data->customer->billigAddress->firstName;
+		$shipping_last_name  = $customer_data->customer->billigAddress->lastName;
+		$shipping_country    = $customer_data->customer->billigAddress->country;
+		$shipping_address    = $customer_data->customer->billigAddress->address;
+		$shipping_city       = $customer_data->customer->billigAddress->city;
+		$shipping_postcode   = $customer_data->customer->billigAddress->postalCode;
+
+		// Standard information
+		$phone = $customer_data->customer->mobilePhoneNumber;
+		$email = $customer_data->customer->email;
+
+		//Populate the fields
+		switch ( $key ) {
+			case 'billing_first_name':
+				return $billing_first_name;
+				break;
+			case 'billing_last_name':
+				return $billing_last_name;
+				break;
+			case 'billing_email':
+				return $email;
+				break;
+			case 'billing_country':
+				return $billing_country;
+				break;
+			case 'billing_address_1':
+				return $billing_address;
+				break;
+			case 'billing_city':
+				return $billing_city;
+				break;
+			case 'billing_postcode':
+				return $billing_postcode;
+				break;
+			case 'billing_phone':
+				return $phone;
+				break;
+			case 'shipping_first_name':
+				return $shipping_first_name;
+				break;
+			case 'shipping_last_name':
+				return $shipping_last_name;
+				break;
+			case 'shipping_email':
+				return $email;
+				break;
+			case 'shipping_country':
+				return $shipping_country;
+				break;
+			case 'shipping_address_1':
+				return $shipping_address;
+				break;
+			case 'shipping_city':
+				return $shipping_city;
+				break;
+			case 'shipping_postcode':
+				return $shipping_postcode;
+				break;
+			case 'shipping_phone':
+				return $phone;
+				break;
+			case 'order_comments':
+				if ( WC()->session->get( 'collector_customer_order_note' ) ) {
+					return WC()->session->get( 'collector_customer_order_note' );
+				} else {
+					return '';
+				}
+				break;
+		} // End switch().
+	}
+	public function set_not_required( $checkout_fields ) {
+		//Set fields to not required, to prevent orders from failing
+		if ( 'dibs_easy' === WC()->session->get( 'chosen_payment_method' ) ) {
+			foreach ( $checkout_fields as $fieldset_key => $fieldset ) {
+				foreach ( $fieldset as $field_key => $field ) {
+					$checkout_fields[ $fieldset_key ][ $field_key ]['required'] = false;
+				}
+			}
+		}
+		return $checkout_fields;
 	}
 
-	public function unclean_url( $good_protocol_url, $original_url, $_context ) {
-		if ( false !== strpos( $original_url, 'data-token' ) ) {
-			remove_filter( 'clean_url','unclean_url' , 10, 3 );
-			$url_parts = parse_url( $good_protocol_url );
-			error_log( $url_parts['scheme'] . '://' . $url_parts['host'] . $url_parts['path'] . ' data-token="' . $this->public_token . '" data-lang="sv"' );
-			return $url_parts['scheme'] . '://' . $url_parts['host'] . $url_parts['path'] . "' data-token='" . $this->public_token . "' data-lang='sv";
-		}
-		return $good_protocol_url;
+	public function process_payment( $order_id, $retry = false ) {
+		$order = wc_get_order( $order_id );
+
+		return array(
+			'result'   => 'success',
+			'redirect' => $this->get_return_url( $order ),
+		);
 	}
 }
