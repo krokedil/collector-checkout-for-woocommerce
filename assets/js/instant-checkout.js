21fa878f6b1b84fe96c0e07be94c6cb7e93a43cc
MichaelBengtsson <michael@krokedil.se>
2017-08-25 08:05:40 +0200

    Instant checkout show/hide on variable products

--- assets/js/instant-checkout.js
+++ assets/js/instant-checkout.js
@@ -1,4 +1,20 @@
 (function ($) {
+    // Check if product is variable product, and wait for variable selection before showing checkout.
+    $(document).ready( function() {
+        if ($('.product-type-variable')[0] && $('.variation_id').val() === '0'){
+            // Product is a variable product and variable is not set
+            $('#collector-instant-iframe-').hide();
+        }
+    });
+    // Check for changes to .variation-id to show the instant checkout
+    $(document).on('change', "input[name='variation_id']", function(){
+        if( $('.variation_id').val() !== '' ) {
+            $('#collector-instant-iframe-').show();
+        } else {
+            console.log('in else');
+            $('#collector-instant-iframe-').hide();
+        }
+    });
     //Listen for even from button press
     $(document).on( 'collectorInstantTokenRequested', function() {
         if( $(".single_add_to_cart_button").val() === '' ){
