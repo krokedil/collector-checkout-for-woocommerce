1effef507d3dae6c038292f1b6d8932dad2d38e9
Niklas Högefjord <niklas@krokedil.se>
2018-11-15 11:17:15 +0100

    Code cleaning + PHP notice fix in collector_order_number()

--- classes/class-collector-checkout-post-checkout.php
+++ classes/class-collector-checkout-post-checkout.php
@@ -5,19 +5,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 class Collector_Checkout_Post_Checkout {
 
 	public function __construct() {
-		$collector_settings = get_option( 'woocommerce_collector_checkout_settings' );
-		$this->manage_orders = $collector_settings['manage_collector_orders'];
+		$collector_settings       = get_option( 'woocommerce_collector_checkout_settings' );
+		$this->manage_orders      = $collector_settings['manage_collector_orders'];
 		$this->display_invoice_no = $collector_settings['display_invoice_no'];
-		
+
 		if ( 'yes' == $this->manage_orders ) {
 			add_action( 'woocommerce_order_status_completed', array( $this, 'collector_order_completed' ) );
 			add_action( 'woocommerce_order_status_cancelled', array( $this, 'collector_order_cancel' ) );
 		}
-		
+
 		if ( 'yes' == $this->manage_orders ) {
-			add_filter ('woocommerce_order_number', array( $this, 'collector_order_number' ), 1000, 2 );
+			add_filter( 'woocommerce_order_number', array( $this, 'collector_order_number' ), 1000, 2 );
 		}
-		
+
 		add_action( 'init', array( $this, 'check_callback' ), 20 );
 	}
 
@@ -32,7 +32,7 @@ class Collector_Checkout_Post_Checkout {
 			$activate_order->request( $order_id );
 		}
 	}
-	
+
 	public function collector_order_cancel( $order_id ) {
 		$order = wc_get_order( $order_id );
 		if ( 'collector_checkout' === $order->get_payment_method() ) {
@@ -44,59 +44,60 @@ class Collector_Checkout_Post_Checkout {
 			$cancel_order->request( $order_id );
 		}
 	}
-	
+
 	/**
 	 * Check for Collector Invoice Status Change (anti fraud system)
 	 **/
 	function check_callback() {
-		if ( strpos( $_SERVER["REQUEST_URI"], 'module/collectorcheckout/invoicestatus' ) !== false ) {
+		if ( strpos( $_SERVER['REQUEST_URI'], 'module/collectorcheckout/invoicestatus' ) !== false ) {
 
-			if( isset( $_GET['InvoiceNo'] ) && isset( $_GET['OrderNo'] ) && isset( $_GET['InvoiceStatus'] ) ) {
+			if ( isset( $_GET['InvoiceNo'] ) && isset( $_GET['OrderNo'] ) && isset( $_GET['InvoiceStatus'] ) ) {
 				Collector_Checkout::log( 'Collector Invoice Status Change callback hit' );
 				$collector_payment_id = wc_clean( $_GET['InvoiceNo'] );
-				$query_args = array(
-					'post_type' => wc_get_order_types(),
+				$query_args           = array(
+					'post_type'   => wc_get_order_types(),
 					'post_status' => array_keys( wc_get_order_statuses() ),
-					'meta_key' => '_collector_payment_id',
-					'meta_value' => $collector_payment_id,
+					'meta_key'    => '_collector_payment_id',
+					'meta_value'  => $collector_payment_id,
 				);
-				$orders = get_posts( $query_args );
-				$order_id = $orders[0]->ID;
-				$order = wc_get_order( $order_id );
+				$orders               = get_posts( $query_args );
+				$order_id             = $orders[0]->ID;
+				$order                = wc_get_order( $order_id );
 
-				if( is_object( $order ) ) {
+				if ( is_object( $order ) ) {
 					// Add order note about the callback
 					$order->add_order_note( sprintf( __( 'Invoice status callback from Collector. New Invoice status: %s', 'collector-checkout-for-woocommerce' ), wc_clean( $_GET['InvoiceStatus'] ) ) );
 					// Set orderstatus
-					if( '1' == $_GET['InvoiceStatus'] ) {
+					if ( '1' == $_GET['InvoiceStatus'] ) {
 						$order->payment_complete( $collector_payment_id );
 					} elseif ( '5' == $_GET['InvoiceStatus'] ) {
 						$order->update_status( 'failed' );
 					}
-					header( "HTTP/1.1 200 Ok" );
+					header( 'HTTP/1.1 200 Ok' );
 				} else {
 					Collector_Checkout::log( 'Invoice status callback from Collector but we could not find the corresponding order in WC. Collector InvoiceNo: ' . wc_clean( $_GET['InvoiceNo'] ) . '. InvoiceStatus: ' . wc_clean( $_GET['InvoiceStatus'] ) . '. OrderNo: ' . wc_clean( $_GET['OrderNo'] ) );
-					header("HTTP/1.0 404 Not Found");
+					header( 'HTTP/1.0 404 Not Found' );
 				}
 			}
-			die();	
+			die();
 		}
 	}
-	
+
 	/**
 	 * Display Collector payment id after WC order number on order overwiev page
 	 **/
 	public function collector_order_number( $order_number, $order ) {
-		if( is_admin() ) {
+		if ( is_admin() ) {
 			// Check if function get_current_screen() exist
 			if ( ! function_exists( 'get_current_screen' ) ) {
 				return $order_number;
 			}
 
 			$current_screen = get_current_screen();
-			if( 'edit-shop_order' == $current_screen->id ) {
-				$collector_payment_id = get_post_meta( $order->id, '_collector_payment_id', true );
-				if( $collector_payment_id ) {
+			if ( 'edit-shop_order' == $current_screen->id ) {
+				$collector_payment_id = null !== get_post_meta( $order->get_id(), '_collector_payment_id', true ) ? get_post_meta( $order->get_id(), '_collector_payment_id', true ) : '';
+				// $collector_payment_id = get_post_meta( $order->get_id(), '_collector_payment_id', true );
+				if ( $collector_payment_id ) {
 					$order_number .= ' (' . $collector_payment_id . ')';
 				}
 			}
