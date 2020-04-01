493a349fe4f28f7d05b4e003a53bd28d9b4fe640
Kristian Andersson <32387117+Kristian-Andersson@users.noreply.github.com>
2020-04-01 15:10:18 +0200

    Version update (1.5.2)

--- collector-checkout-for-woocommerce.php
+++ collector-checkout-for-woocommerce.php
@@ -8,14 +8,14 @@
  * Plugin Name:     Collector Checkout for WooCommerce
  * Plugin URI:      https://krokedil.se/collector/
  * Description:     Extends WooCommerce. Provides a <a href="https://www.collector.se/" target="_blank">Collector Checkout</a> checkout for WooCommerce.
- * Version:         1.5.1
+ * Version:         1.5.2
  * Author:          Krokedil
  * Author URI:      https://krokedil.se/
  * Text Domain:     collector-checkout-for-woocommerce
  * Domain Path:     /languages
  *
  * WC requires at least: 3.0.0
- * WC tested up to: 3.9.2
+ * WC tested up to: 4.0.1
  *
  * Copyright:       © 2017-2020 Krokedil.
  * License:         GNU General Public License v3.0
@@ -28,7 +28,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 define( 'COLLECTOR_BANK_PLUGIN_DIR', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
 define( 'COLLECTOR_BANK_PLUGIN_URL', untrailingslashit( plugin_dir_url( __FILE__ ) ) );
-define( 'COLLECTOR_BANK_VERSION', '1.5.1' );
+define( 'COLLECTOR_BANK_VERSION', '1.5.2' );
 
 if ( ! class_exists( 'Collector_Checkout' ) ) {
 	class Collector_Checkout {
