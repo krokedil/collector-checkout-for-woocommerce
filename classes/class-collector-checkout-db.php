4c388f50178eb56fce2bdc8e6ab3eab59cb1b310
Kristian Andersson <32387117+Kristian-Andersson@users.noreply.github.com>
2020-04-23 12:23:18 +0200

    Add database class and file

---
+++ classes/class-collector-checkout-db.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Class for handling the Database table
+ *
+ * @package collector-checkout-for-woocommerce/Classes
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Class for handling the Database table
+ */
+class Collector_Checkout_DB {
+
+
+}
