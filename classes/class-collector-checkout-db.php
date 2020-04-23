a5d979e596e970ebf8e670b9efee61039dbc9c01
Kristian Andersson <32387117+Kristian-Andersson@users.noreply.github.com>
2020-04-23 17:39:49 +0200

    Add basic sql functionality for Collector_Checkout_DB class

--- classes/class-collector-checkout-db.php
+++ classes/class-collector-checkout-db.php
@@ -11,6 +11,133 @@ defined( 'ABSPATH' ) || exit;
  * Class for handling the Database table
  */
 class Collector_Checkout_DB {
+	/**
+	 * The name of the database table.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string
+	 */
+	private static $table_name = 'collector_data';
 
+	/**
+	 * Setup table.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return bool
+	 */
+	public static function setup_table() {
+		global $wpdb;
+		$prefix     = $wpdb->prefix;
+		$table_name = $prefix . self::$table_name;
+		// Table already exists, return true.
+		if ( self::table_exists( $table_name ) ) {
+			return true;
+		}
+
+		if ( ! self::create_table( $table_name ) ) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Removes the database table.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return bool
+	 */
+	public static function remove_table() {
+		global $wpdb;
+		$prefix     = $wpdb->prefix;
+		$table_name = $prefix . self::$table_name;
+		if ( self::table_exists( $table_name ) ) {
+            return $wpdb->query( 'DROP TABLE IF EXISTS ' . $table_name ); //phpcs:ignore
+		}
+
+	}
+
+
+	/**
+	 * Checks if the table exists in the database already.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $table_name The name of the table.
+	 * @return bool
+	 */
+	private static function table_exists( $table_name ) {
+		global $wpdb;
+		return $wpdb->get_var( $wpdb->prepare( 'SHOW TABLES LIKE %s', $wpdb->esc_like( $table_name ) ) ) === $table_name;
+	}
+
+	/**
+	 * Creates the table in the database.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $table_name The name of the new table.
+	 * @return bool
+	 */
+	private static function create_table( $table_name ) {
+		global $wpdb;
+		$charset_collate = $wpdb->get_charset_collate();
+		$sql             = "CREATE TABLE {$table_name} (
+			`data` MEDIUMTEXT NOT NULL
+		) $charset_collate;";
+		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+		$result = dbDelta( $sql );
+
+		return true;
+	}
+
+	/**
+	 * Creates a data entry for the table.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $args The data for the row.
+	 * @return int
+	 */
+	public static function create_data_entry( $args = array() ) {
+		if ( empty( $args ) || ! is_array( $args ) ) {
+			return false;
+		}
+
+		global $wpdb;
+
+		$table_name = $wpdb->prefix . self::$table_name;
+
+		$wpdb->insert( // phpcs:ignore
+			$table_name,
+			array(
+				'data' => $args['data'], // TODO: json data. Check sanitization.
+			),
+			array(
+				'%s',
+			)
+		);
+
+		return $wpdb->insert_id;
+	}
+
+	/**
+	 * Gets the data entry for the id.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return object
+	 */
+	public static function get_data_entry() {
+		global $wpdb;
+		$table_name = $wpdb->prefix . self::$table_name;
+		$query      = $wpdb->query( "SELECT * FROM `{$table_name}` LIMIT 1" ); //phpcs:ignore
+		$data       = $wpdb->get_results( $query ); // phpcs:ignore
+
+		return $data[0];
+	}
 
 }
