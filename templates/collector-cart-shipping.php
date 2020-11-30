a997415aadf9ea0af944fb1801c0e18fbb614d23
Niklas Högefjord <niklas@krokedil.se>
2020-11-30 15:30:21 +0100

    Cart shipping template tweak

--- templates/collector-cart-shipping.php
+++ templates/collector-cart-shipping.php
@@ -11,15 +11,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 $shipping_id = WC()->session->get( 'chosen_shipping_methods' )[0];
 $packages    = WC()->shipping->get_packages();
+$rate        = null;
 foreach ( $packages as $package ) {
 	if ( isset( $package['rates'] ) && isset( $package['rates'][ $shipping_id ] ) ) {
 		$rate = $package['rates'][ $shipping_id ];
 		break;
 	}
 }
-?>
-
-<tr class="woocommerce-shipping-totals shipping">
-	<th><?php esc_html_e( 'Shipping', 'woocommerce' ); ?></th>
-	<td data-title="Shipping" class="kco-shipping"><?php echo wc_cart_totals_shipping_method_label( $rate ); // WPCS: XSS ok. ?></td>
-</tr>
+if ( ! empty( $rate ) ) {
+	?>
+	<tr class="woocommerce-shipping-totals shipping">
+		<th><?php esc_html_e( 'Shipping', 'woocommerce' ); ?></th>
+		<td data-title="Shipping" class="kco-shipping"><?php echo wc_cart_totals_shipping_method_label( $rate ); // WPCS: XSS ok. ?></td>
+	</tr>
+	<?php
+}
