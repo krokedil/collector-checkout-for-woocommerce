f696300327fdef7f2921434eda1b424868b17f03
Niklas Högefjord <niklas@krokedil.se>
2021-10-07 13:56:22 +0200

    Enable B2B/B2C switcher for checkout v2 again

--- includes/collector-checkout-for-woocommerce-functions.php
+++ includes/collector-checkout-for-woocommerce-functions.php
@@ -136,16 +136,12 @@ function collector_wc_show_another_gateway_button() {
  * Only available for Checkout version 1.0.
  */
 function collector_wc_show_customer_type_switcher() {
-	$collector_settings = get_option( 'woocommerce_collector_checkout_settings' );
-	$checkout_version   = isset( $collector_settings['checkout_version'] ) ? $collector_settings['checkout_version'] : 'v1';
-	if ( 'collector-b2c-b2b' === wc_collector_get_available_customer_types() && 'v1' === $checkout_version ) {
-		?>
-		<ul class="collector-checkout-tabs">
-			<li class="tab-link current" data-tab="b2c"><?php _e( 'Person', 'collector-checkout-for-woocommerce' ); ?></li>
-			<li class="tab-link" data-tab="b2b"><?php _e( 'Company', 'collector-checkout-for-woocommerce' ); ?></li>
-		</ul>
-		<?php
-	}
+	?>
+	<ul class="collector-checkout-tabs">
+		<li class="tab-link current" data-tab="b2c"><?php _e( 'Person', 'collector-checkout-for-woocommerce' ); ?></li>
+		<li class="tab-link" data-tab="b2b"><?php _e( 'Company', 'collector-checkout-for-woocommerce' ); ?></li>
+	</ul>
+	<?php
 }
 
 /**
