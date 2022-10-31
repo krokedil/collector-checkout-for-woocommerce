a8e441d1bec77d78128a270b6f0dba3f6ee11f88
Montazar Al-Jaber <github.v0its@slmail.me>
2022-10-31 22:06:24 +0100

    Remove unused variable

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -66,7 +66,6 @@
     
     // Customer updated - event triggered when customer changes address in Collector iframe
     document.addEventListener("collectorCheckoutCustomerUpdated", function(){
-        var url = window.location.href;
         // Check that this only happens in checkout page. Don't do it on thank you page
         if ( wc_collector_checkout.is_thank_you_page === 'no' ) {
             window.collector.checkout.api.suspend();
