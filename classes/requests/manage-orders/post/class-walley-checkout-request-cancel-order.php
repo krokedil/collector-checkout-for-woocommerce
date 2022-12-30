219617663c37dd661040d9c0607507d227abb142
Niklas Högefjord <niklas@krokedil.se>
2022-12-30 10:51:50 +0100

    Add cancel order request class

---
+++ classes/requests/manage-orders/post/class-walley-checkout-request-cancel-order.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Class for the request to cancel an order.
+ *
+ * @package Collector_Checkout/Classes/Requests/POST
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Walley_Checkout_Request_Cancel_Order class.
+ */
+class Walley_Checkout_Request_Cancel_Order extends Walley_Checkout_Request_Post {
+
+	/**
+	 * Class constructor.
+	 *
+	 * @param array $arguments The request arguments.
+	 */
+	public function __construct( $arguments ) {
+		parent::__construct( $arguments );
+		$this->log_title = 'Cancel order';
+	}
+
+	/**
+	 * Get the request url.
+	 *
+	 * @return string
+	 */
+	protected function get_request_url() {
+		$walley_id = get_post_meta( $this->arguments['order_id'], '_collector_order_id', true );
+		return $this->get_api_url_base() . "/manage/orders/{$walley_id}/cancel";
+	}
+
+	/**
+	 * Get the body for the request.
+	 *
+	 * @return array
+	 */
+	protected function get_body() {
+		$order_id = $this->arguments['order_id'];
+		$order    = wc_get_order( $order_id );
+		$body     = array(
+			'amount' => $order->get_total(),
+		);
+
+		return apply_filters( 'coc_order_cancel_args', $body, $this->arguments['order_id'] );
+	}
+}
