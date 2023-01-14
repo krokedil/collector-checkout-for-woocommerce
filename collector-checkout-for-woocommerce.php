1a87b612bf903204e21a9326f56a1e6bc1b9f57a
Martin Lindahl <84283336+krokedilmartin@users.noreply.github.com>
2023-01-14 18:38:53 +0100

    Bump tested WC version

--- collector-checkout-for-woocommerce.php
+++ collector-checkout-for-woocommerce.php
@@ -15,7 +15,7 @@
  * Domain Path:     /languages
  *
  * WC requires at least: 5.0.0
- * WC tested up to: 7.2.3
+ * WC tested up to: 7.3.0
  *
  * Copyright:       © 2017-2023 Krokedil.
  * License:         GNU General Public License v3.0
