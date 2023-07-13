be19435c105c374b5bb3a6312d285bba852832f4
Niklas Högefjord <niklas@krokedil.se>
2023-07-13 09:47:53 +0200

    Fix saving of customer phone and email to Woo order for B2B with new API

--- classes/class-walley-checkout-session.php
+++ classes/class-walley-checkout-session.php
@@ -24,9 +24,6 @@ class Walley_Checkout_Session {
 		$customer_address                     = array();
 		$customer_address['billing_country']  = $walley_order['data']['countryCode'] ?? '';
 		$customer_address['shipping_country'] = $walley_order['data']['countryCode'] ?? '';
-		$customer_address['billing_email']    = $walley_order['data']['customer']['email'] ?? '';
-		$customer_address['billing_phone']    = $walley_order['data']['customer']['mobilePhoneNumber'] ?? '';
-		$customer_address['shipping_phone']   = $walley_order['data']['customer']['deliveryContactInformation']['mobilePhoneNumber'] ?? '';
 
 		if ( 'BusinessCustomer' === $walley_order['data']['customerType'] ) {
 			$customer_address['billing_first_name'] = $walley_order['data']['businessCustomer']['firstName'] ?? '';
@@ -40,6 +37,9 @@ class Walley_Checkout_Session {
 			$customer_address['shipping_city']       = $walley_order['data']['businessCustomer']['deliveryAddress']['city'] ?? '';
 			$customer_address['shipping_address_1']  = $walley_order['data']['businessCustomer']['deliveryAddress']['address'] ?? '';
 			$customer_address['shipping_postcode']   = $walley_order['data']['businessCustomer']['deliveryAddress']['postalCode'] ?? '';
+
+			$customer_address['billing_email'] = $walley_order['data']['businessCustomer']['email'] ?? '';
+			$customer_address['billing_phone'] = $walley_order['data']['businessCustomer']['mobilePhoneNumber'] ?? '';
 		} else {
 			$customer_address['billing_first_name'] = $walley_order['data']['customer']['billingAddress']['firstName'] ?? '';
 			$customer_address['billing_last_name']  = $walley_order['data']['customer']['billingAddress']['lastName'] ?? '';
@@ -52,6 +52,10 @@ class Walley_Checkout_Session {
 			$customer_address['shipping_city']       = $walley_order['data']['customer']['deliveryAddress']['city'] ?? '';
 			$customer_address['shipping_address_1']  = $walley_order['data']['customer']['deliveryAddress']['address'] ?? '';
 			$customer_address['shipping_postcode']   = $walley_order['data']['customer']['deliveryAddress']['postalCode'] ?? '';
+
+			$customer_address['billing_email']  = $walley_order['data']['customer']['email'] ?? '';
+			$customer_address['billing_phone']  = $walley_order['data']['customer']['mobilePhoneNumber'] ?? '';
+			$customer_address['shipping_phone'] = $walley_order['data']['customer']['deliveryContactInformation']['mobilePhoneNumber'] ?? '';
 		}
 
 		return $customer_address;
