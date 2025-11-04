76b8ea5f0f2900fec8fd5aa7e1b87bb080456195
Montazar <github.v0its@slmail.me>
2025-11-04 06:20:21 +0100

    Migrate Redlight and nShift profiles

--- classes/class-collector-checkout-gateway.php
+++ classes/class-collector-checkout-gateway.php
@@ -166,20 +166,33 @@ class Collector_Checkout_Gateway extends WC_Payment_Gateway {
 				continue;
 			}
 
-			$saved_profile = $this->settings[ "collector_custom_profile_$country" ] ?? '';
+			$saved_profile = $this->settings[ "collector_custom_profile_$country" ] ?? null;
 			if ( empty( $saved_profile ) ) {
 				$this->update_option( "walley_custom_profile_$country", 'no' );
 				continue;
 			}
 
-			foreach ( $profiles as $profile ) {
-				// Migrate the old setting to the corresponding new profile based on a substring match.
-				if ( strpos( strtolower( $profile ), strtolower( $saved_profile ) ) ) {
-					$this->update_option( "walley_custom_profile_$country", $profile );
-					break; // with next country.
-				} else {
-					// This is an unknown profile.
-					$this->update_option( "walley_custom_profile_$country", 'no' );
+			// Normalize by making to lowercase and removing non-alphabetical characters.
+			$saved_profile = preg_replace( '/[^a-z]/', '', strtolower( $saved_profile ) );
+
+			// We'll use the non-recurring profiles since when this is released, no merchant should have recurring profiles active.
+			if ( strpos( $saved_profile, 'redlight' ) !== false ) {
+				$this->update_option( 'Shipping-Redlight' );
+				break;
+			} elseif ( strpos( $saved_profile, 'nshift' ) !== false ) {
+				$this->update_option( 'Shipping-nShift' );
+				break;
+			} else {
+				foreach ( $profiles as $profile ) {
+					$profile = preg_replace( '/[^a-z]/', '', strtolower( $profile ) );
+					if ( false !== strpos( $profile, $saved_profile ) ) {
+						// Migrate the old setting to the corresponding new profile based on a substring match.
+						$this->update_option( "walley_custom_profile_$country", $profile );
+						break; // with next country.
+					} else {
+						// This is an unknown profile.
+						$this->update_option( "walley_custom_profile_$country", 'no' );
+					}
 				}
 			}
 		}
