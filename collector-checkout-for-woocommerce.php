7190a044dc5b95218820b778e116d636c455038c
Montazar <github.mdtk4@passmail.com>
2026-05-28 13:37:35 +0200

    Bump wc tested up to

--- collector-checkout-for-woocommerce.php
+++ collector-checkout-for-woocommerce.php
@@ -16,7 +16,7 @@
  * Requires Plugins: woocommerce
  *
  * WC requires at least: 6.0.0
- * WC tested up to: 10.7.0
+ * WC tested up to: 10.8.1
  *
  * Copyright: © 2017-2026 Krokedil.
  * License: GNU General Public License v3.0
