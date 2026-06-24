ad282d7f6e2f64106b2bacfd2fec7fd087fb62c3
robin-krokedil <robin@krokedil.se>
2026-06-24 11:14:11 +0200

    Bumped WC tested version to 10.9.0 after test

--- collector-checkout-for-woocommerce.php
+++ collector-checkout-for-woocommerce.php
@@ -16,7 +16,7 @@
  * Requires Plugins: woocommerce
  *
  * WC requires at least: 6.0.0
- * WC tested up to: 10.8.1
+ * WC tested up to: 10.9.0
  *
  * Copyright: © 2017-2026 Krokedil.
  * License: GNU General Public License v3.0
